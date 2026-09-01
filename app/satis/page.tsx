03cfae5a8272d597038432bb77f4ee7b8f280f7c
Özkan Türkoğlu <GitHub hesabnda kullandn e-posta>
2026-09-01 16:11:31 +0300

    Kendi sandvicini yap fiyat yazisi kisaltildi

--- app/satis/page.tsx
+++ app/satis/page.tsx
@@ -3050,7 +3050,7 @@ export default function Satislar() {
                         fontSize: "18px",
                       }}
                     >
-                      Fiyatı Elle Gir
+                      Fiyat Gir
                     </strong>
                   </button>
                 )}
